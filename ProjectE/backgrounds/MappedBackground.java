7b5321a8e889bce58c094294545df7d934ac33e9
melan <melan@10.200.24.154>
2022-04-25 13:17:46 -0600

    mapped background edited

--- ProjectE/backgrounds/MappedBackground.java
+++ ProjectE/backgrounds/MappedBackground.java
@@ -51,24 +51,103 @@ public class MappedBackground implements Background {
 		{0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0}
 	};
-
-	@Override
-	public Tile getTile(int col, int row) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public MappedBackground() {
+		try {
+			this.barrier = ImageIO.read(new File("res/8fc992.png"));
+			this.path = ImageIO.read(new File("res/ba69c7.png"));
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.toString());
+		}
+		
+		maxRows = map.length - 1;
+		maxCols = map[0].length - 1;
 	}
 
 	@Override
-	public int getCol(double x) {
-		// TODO Auto-generated method stub
+	public Tile getTile(int col, int row) {
+		Image image = null;
+		
+		if (row < 0 || row > maxRows || col < 0 || col > maxCols) {
+			image = null;
+		}
+		else if(map[row][col] == 0)
+		{
+			image = barrier;
+		}
+		else if(map[row][col] == 1)
+		{
+			image = path;
+		}
+		else
+		{
+			image = null;
+		}
+		
+		int x = (col * TILE_WIDTH) - (AnimationFrame.SCREEN_WIDTH / 2);
+		int y = (row * TILE_HEIGHT) - (AnimationFrame.SCREEN_HEIGHT / 2);
+		
+		Tile newTile = new Tile(image, x, y, TILE_WIDTH, TILE_HEIGHT, false);
+		
+		return newTile;
+	}
+
+	public int getHorizontal(int x) {
+		//which tile is x sitting at?
 		return 0;
+	}
+	
+	@Override
+	public int getCol(double x) {
+		//which col is x sitting at?
+		int col = 0;
+		if (TILE_WIDTH != 0) {
+			col = (int) (x / TILE_WIDTH);
+			if (x < 0) {
+				return col - 1;
+			}
+			else {
+				return col;
+			}
+		}
+		else {
+			return 0;
+		}
 	}
 
 	@Override
 	public int getRow(double y) {
-		// TODO Auto-generated method stub
-		return 0;
+		//which row is y sitting at?
+		int row = 0;
+		
+		if (TILE_HEIGHT != 0) {
+			row = (int) (y / TILE_HEIGHT);
+			if (y < 0) {
+				return row - 1;
+			}
+			else {
+				return row;
+			}
+		}
+		else {
+			return 0;
+		}
 	}
+	
+	public ArrayList<DisplayableSprite> getBarriers() {
+		ArrayList<DisplayableSprite> barriers = new ArrayList<DisplayableSprite>();
+		for (int row = 0; row < map[0].length; row++) {
+			for (int col = 0; col < map.length; col++) {
+				if (map[col][row] == 0) {
+					barriers.add(new BarrierSprite((row * TILE_WIDTH) - (AnimationFrame.SCREEN_WIDTH / 2), (col * TILE_HEIGHT) - (AnimationFrame.SCREEN_HEIGHT / 2), ((row + 1) * TILE_WIDTH) - (AnimationFrame.SCREEN_WIDTH / 2), ((col + 1) * TILE_HEIGHT) - (AnimationFrame.SCREEN_HEIGHT / 2)));
+				}
+			}
+		}
+		return barriers;
+	}
+	
 
 	@Override
 	public double getShiftX() {
@@ -93,5 +172,4 @@ public class MappedBackground implements Background {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
