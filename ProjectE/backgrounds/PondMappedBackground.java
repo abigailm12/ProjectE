1e78c7c1db51a85c44ce87d9ca73372744f23c80
904167502 <904167502@L829WD210024.edu.cbe.ab.ca>
2022-06-02 15:01:12 -0600

    misc

--- ProjectE/backgrounds/PondMappedBackground.java
+++ ProjectE/backgrounds/PondMappedBackground.java
@@ -95,7 +95,7 @@ public class PondMappedBackground implements Background {
 		} else if (map[col][row] == 0) {
 			image = grass;
 		} else if (map[col][row] == 1) {
-			int var = (int) Math.random() + 2;
+			int var = (int) (Math.random()*2 + 1);
 			if (var == 1) {
 				image = waterAlternate1;
 			} else if (var == 2) {
